a82b443362620c40315ce01be6190f2d80db3561
CAMT <benyapa_sripa@cmu.ac.th>
2024-08-10 11:04:37 +0700

    lab01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navber from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <Navber/> 
+      <h1 style={{ padding: '4rem 0', margin: '0 auto', maxWidth: 1200 }}>
+        Hello React
+      </h1>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (<header>
+  <a href="/" className="brand">
+    Trendie
+  </a>
+  <a href="/create-product">Create product</a>
+ 
+</header>);
+}
+
+export default App;
